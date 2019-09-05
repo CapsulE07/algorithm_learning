e3ed794f9532f02bdaa478dfb2f63f261ad57c6b
Dominic <yinxing.dky@raycloud.com>
2019-09-05 18:07:53 +0800

    ''

--- KMP/kmp.py
+++ KMP/kmp.py
@@ -1,2 +1,21 @@
 def kmp(input, pattern):
+    i = 0
+    j=0
+    while (i < len(input) and j < len(pattern)):
+        if input[i]==pattern[j]:
+            i +=1
+            j +=1
+        else:
+            j=next[j]
+
+    if j==len(pattern):
+        return i-j
+    else:
+        return -1
+
+def get_next(pattern):
+    next[0] = -1
+    i,j=0,-1
+    while i < len(pattern):
+        if 
     
